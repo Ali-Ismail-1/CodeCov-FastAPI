1e10fd340ba8a6f4b9bb82c923775260638ac6a5
Ali Ismail <aliismaildeveloper@gmail.com>
2024-08-12 19:26:39 -0700

    Added endpoints in main to test coverage from codecov

--- app/main.py
+++ app/main.py
@@ -7,6 +7,15 @@ app = FastAPI()
 async def read_root():
     return {"message": "Hello World"}
 
+@app.get("/test")
+async def read_root():
+    return {"message": "This is a test"}
+
+@app.get("/about")
+async def read_root():
+    return {"message": "About endpoint"}
+
+
 @app.get("/add/{a}/{b}")
 async def add(a: int, b: int):
     return {"result": a + b}
